b9a69098e4c7d689e59bd7ec6a32bfdf16f06d92
Robert Hassenmayer <rob@localhost.localdomain>
2022-05-10 04:57:43 -0400

    mkcmd can now turn python scripts into commands

--- mkcmd.sh
+++ mkcmd.sh
@@ -2,11 +2,24 @@
 
 #add conditionals for possible binary directories
 #add --help option
-#add a third argument for choice of bin dir or if /usr/bin not found
+#add a third argument for a choice of bin dir or if /usr/bin not found
+#make .py version then put .py and .sh version in  seperate functions to be called in makecommand() depending on option
+#make seperate move command
 
-makecommand () {
-    case $1 in
-        "-h" | "--help")
+
+
+makebash () {
+    shc -f $2 -o $1
+    rm ./$2.x.c
+    mv $1 /usr/bin
+}
+
+makepython () {
+    chmod 755 $2
+    cp $2 /usr/bin/$1
+}
+
+makecommandhelpmenu () {
         echo "mkcmd takes a script/file and converts it into a bash shell command."
         echo ""
         echo "      Use: sudo mkcmd <new_command_name> <script/file>"
@@ -17,15 +30,31 @@ makecommand () {
         echo ""
         echo "-h, --help:"
         echo "      Print this menu to shell."
+}
+
+makecommand () {
+    SUCCESS="[+] Command created successfully"
+    case $1 in
+        "-h" | "--help")
+        makecommandhelpmenu
         ;;
         *)
-        if [ -z $1 ]
+        if [[ -n $2 ]]
+        then 
+            FILE_TYPE=$(file $2)
+        fi
+
+        if [[ -z $1 ]]
         then
             echo "Type mkcmd -h or mkcmd --help for options and examples."
-        else
-            shc -f $2 -o $1
-            rm ./$2.x.c
-            mv $1 /usr/bin
+        elif [[ $FILE_TYPE == *" Bourne-Again shell script"* ]]
+        then
+            makebash $1 $2
+            echo $SUCCESS
+        elif [[ $FILE_TYPE == *"Python script"* ]]
+        then
+            makepython $1 $2
+            echo $SUCCESS
         fi
         ;;
     esac
